ed5ee124adc62b3b9a7dced19e0d8bec6a41a402
Deven8378 <devenpatel8378@gmail.com>
2023-01-31 22:47:12 -0500

    fixed hopefully

--- app/core/App.php
+++ app/core/App.php
@@ -1,47 +1,45 @@
 <?php
 namespace app\core;
-class App {
-	function __construct() {
-		//this is where we want to route the request to the appropriate classes methods
-		//we wish to route requests to /controller/method
-		$request = $this ->parseUrl($_GET['url'] ?? '');
-		
-		//	var_dump(request);
 
-		//defaults
-		$controller = 'main';
+class App{
+
+	function __construct(){
+		//this is where we want to route the requests to the appropriate classes/methods
+		// we wish to route requests to /controller/method
+		$request = $this->parseUrl($_GET['url'] ?? '');
+//		var_dump($request);
+
+		//default controller and method
+		$controller = 'Main';
 		$method = 'index';
 		$params = [];
 
-		//is the requested controller in the controllers folder?
-		if (file_exists('app/controllers' . $request[0] . '.php')) {
-			echo "the $request[0] controller exists";
-
+		//is the requested controller in our controllers folder?
+		if(file_exists('app/controllers/' . $request[0] . '.php'))
+		{
 			$controller = $request[0];
-		//	$controller = new main();
-			
-			//ToDO: remove the $request[0] element
+			//$controller = new Main();
+			//remove the $request[0] element
 			unset($request[0]);
 		}
 		$controller = 'app\\controllers\\' . $controller;
 		$controller = new $controller;
 
-		if (isset($request[1] && method_exists($controller, $request[1])) {
+		if(isset($request[1]) && method_exists($controller, $request[1])){
 			$method = $request[1];
-			//ToDo: remove $request[1]
+			//remove the $request[1] element
 			unset($request[1]);
 		}
-		$controller->$method();
-	
+
 		$params = array_values($request);
 
-		//call the controller method with parameters
-		call_user_func_array([$controller, $method],$params);
-		
+		//Call the controller method with parameters
+		call_user_func_array([$controller, $method], $params);
 	}
 
-	function parseUrl($url) {
-
-		return explode('/',trim($url,'/'));
+	function parseUrl($url){
+		return explode('/', trim($url, '/'));
 	}
+
+
 }
