b09825d6d49a0838953eaa80d988c9f073bafe8f
Deven Patel <2171750@edu.vaniercollege.qc.ca>
2023-02-01 11:40:32 -0500

    forth course

--- app/controllers/main.php
+++ app/controllers/main.php
@@ -13,4 +13,21 @@ class Main extends \app\core\Controller{
 	function greetings($name = "Carl") { //name is an optional parameter
 		$this->view('Main/greetings', $name);
 	}
+
+	function logUser() {
+		if (isset($_POST['action'])) {
+			//data is sent
+			//open the log.txt file for appending
+			//ToDo: lock the file for reserved access
+			$fh = fopen('log.txt', 'a');
+			fwrite($fh, "$_POST[name] has visited!\n");
+			fclose($fh);
+			header('location:/Main/logUser');
+
+		} else {
+			// no data submitted: the user needs to see the form
+			$this->view('Main/logUser');
+
+		}
+	}
 }
