784d8b5ceae3c54ad1ea186d1dbe1203fa4cebef
Deven Patel <2171750@edu.vaniercollege.qc.ca>
2023-02-21 11:52:39 -0500

    added more sql stuff prob not worky

---
+++ app/models/Message.php
@@ -0,0 +1,46 @@
+<?php
+namespace app\models;
+
+class Message extends \app\core\Model {
+	public $message_id;
+	public $sender;
+	public $receiver;
+	public $message;
+	public $timestamp;
+
+	public function insert() {
+		# the data we want to insert
+		$SQL = "INSERT INTO message (message,sender,receiver) value (:message,:sender,:receiver)";
+
+		$STH = $this->connection->prepare($SQL);
+		$data = ['sender'=>$this->sender,
+		 		'receiver'=>$this->receiver,
+		 		'message'=>$this->message];
+		$STH->execute($data);
+
+		$this->message_id = $this->connection->lastInsertId();
+		//TODO: if needed ge the timestamp
+	}
+
+	public function getAllForUser($user_id) {
+		# the data we want to insert
+		$SQL = "SELECT * FROM message WHERE sender=:sender OR receiver=:receiver";
+
+		$STH = $this->connection->prepare($SQL);
+		$STH->execute();
+		$data = ['receiver'=>$user_id, 'sender'->$user_id];
+		$STH->setFetchMode(\PDO::FETCH_CLASS,'app\\models\\Message');
+		return $STH->fetchAll();
+
+	public function delete($message_id, $user_id) {
+		//TODO: only allow this operation as a receiver
+		$SQL = "DELETE FROM message WHERE message_id=:message_id AND :receiver";
+		$STH = $this->connection->prepare($SQL);
+		$data = ['message_id'=>$message_id, 'receiver'=>$user_id];
+		$STH->execute($data);
+	}
+
+		
+
+
+}
--- app/views/User/profile.php
+++ app/views/User/profile.php
@@ -1,5 +1,34 @@
 <?php $this->view('shared/header','Register your account'); ?>
 User Profile!!!
+
+<h1> Messages</h1>
+
+<h2> My messages </h2>
+
+<h2> Send a message </h2>
+<table> <tr> <th> sender </th> <th> receiver </th> <th> message </th> <th> time </th> <th> actions </th> </tr>
+<?php
+//display all messages
+foreach ($data as $message) {
+	echo " <tr>
+	<td> $message->sender </td>
+	 <td> $message->receiver </td>
+	  <td> $message->message </td>
+	   <td> $message->timestamp </td>
+	    <td> <a href='/User/messageDelete/$message->message_id'> DELETE </a> </td>
+	     </tr>";
+}
+?>
+</table>
+
+<p> Send a message using the following form </p> 
+<form action="/User/sendMessage" method='post'>
+	<label> To: <input type="text" name="receiver"> </label>
+	<br/>
+	<label> Message: <textarea name="message"> </textarea>
+		<br/> <input type="submit" name="action" value="Send Message">
+</form>
+
 <a href='/User/logout'> Logout</a>
 
 <?php $this->view('shared/footer'); ?>
